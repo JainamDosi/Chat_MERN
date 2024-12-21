4252b736fc7db9383592ca60029d9b5dbe7f0d5e
Jainam <127018175+JainamDosi@users.noreply.github.com>
2024-12-21 18:42:55 +0530

    minor fixes2

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -1,17 +1,15 @@
-import express from 'express';
-import { loginUser, logoutUser, signupUser, updateUSer,checkAuth } from '../controllers/auth.controller.js';
-import { protectRoute } from '../middleware/auth.middleware.js';
+import express from "express";
+import { checkAuth, login, logout, signup, updateProfile } from "../controllers/auth.controller.js";
+import { protectRoute } from "../middleware/auth.middleware.js";
 
-const router=express.Router();
+const router = express.Router();
 
-router.post("/login",loginUser);
+router.post("/signup", signup);
+router.post("/login", login);
+router.post("/logout", logout);
 
-router.post("/signup",signupUser);
+router.put("/update-profile", protectRoute, updateProfile);
 
-router.post("/logout",logoutUser);
-
-router.put("/update-profile",protectRoute,updateUSer);
-
-router.get("/check",protectRoute,checkAuth);
+router.get("/check", protectRoute, checkAuth);
 
 export default router;
